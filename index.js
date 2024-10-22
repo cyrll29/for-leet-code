cb9c657119d1739236dbb774ff7c0240be01d8cf
cyrll29 <75314757+cyrll29@users.noreply.github.com>
2024-10-22 22:17:58 +0800

    convert roman numerals to integers

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const palindromeNumber = (num) => {
+    // Convert number into array then reverses it then converting it to array again
     let y = parseInt(num.toString().split('').map(Number).reverse().join(''))
 
     return y === num ? true : false;
+}
+
+const romanToInt = (s) => {
+    let res = 0;
+    const roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    for (let i = 0; i < s.length - 1; i++) {
+        if (roman[s[i]] < roman[s[i + 1]]) {
+            res -= roman[s[i]];
+        } else {
+            res += roman[s[i]];
+        }
+    }
+
+    return res + roman[s[s.length - 1]]; 
 }
