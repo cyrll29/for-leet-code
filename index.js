35f9c514005c026a6c620cd7fadc68fe5053ba33
cyrll29 <75314757+cyrll29@users.noreply.github.com>
2024-10-24 23:33:33 +0800

    searchInsert algo

--- index.js
+++ index.js
@@ -64,3 +64,19 @@ const strStr = (haystack, needle) => {
 
     return int;
 }
+
+const searchInsert = (nums, target) => {
+    let result = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] < target) {
+            result = i + 1;
+        } else if(nums[i] === target) {
+            result = i;
+        } else {
+            continue;
+        }
+    }
+
+    return result;
+}
