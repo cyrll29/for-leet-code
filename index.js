7c3e316d411be77cb8290a20afc4bcf65bf571fd
cyrll29 <75314757+cyrll29@users.noreply.github.com>
2024-10-26 14:50:07 +0800

    plusOne

--- index.js
+++ index.js
@@ -85,3 +85,17 @@ const lengthOfLastWord = (s) => {
     const arr = s.trim().split(' ');
     return arr[arr.length - 1].length;
 }
+
+const plusOne = (digits) => {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] + 1 !== 10) {
+            digits[i] += 1;
+            return digits;
+        }
+        digits[i] = 0;
+        if (i === 0) {
+            digits.unshift(1);
+            return digits;
+        }
+    }    
+}
