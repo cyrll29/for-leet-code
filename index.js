1c65a163881eaa31ddd722ade473ff2576d6b7b6
cyrll29 <75314757+cyrll29@users.noreply.github.com>
2024-10-28 12:46:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -103,3 +103,22 @@ const plusOne = (digits) => {
 const mySqrt = (x) => {
     return Math.floor(Math.sqrt(x));;
 };
+
+const merge = (nums1, m, nums2, n) => {
+    let midx = m - 1;
+    let nidx = n - 1;
+    let right = m + n - 1;
+
+    while (nidx >= 0) {
+        if (midx >= 0 && nums1[midx] > nums2[nidx]) {
+            nums1[right] = nums1[midx];
+            midx--;
+        } else {
+            nums1[right] = nums2[nidx];
+            nidx--;
+        }
+        right--;
+    }    
+}
+
+merge();
