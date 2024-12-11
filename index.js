35ac4fa39aceeafed847a5ffc9aba66b4a22a5ba
cyrll29 <75314757+cyrll29@users.noreply.github.com>
2024-12-11 12:07:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -144,3 +144,47 @@ const getOneBits = (decimal) => {
     return res;
 }
 
+const alphabet = {
+    1: 'A',
+    2: 'B',
+    3: 'C',
+    4: 'D',
+    5: 'E',
+    6: 'F',
+    7: 'G',
+    8: 'H',
+    9: 'I',
+    10: 'J',
+    11: 'K',
+    12: 'L',
+    13: 'M',
+    14: 'N',
+    15: 'O',
+    16: 'P',
+    17: 'Q',
+    18: 'R',
+    19: 'S',
+    20: 'T',
+    21: 'U',
+    22: 'V',
+    23: 'W',
+    24: 'X',
+    25: 'Y',
+    26: 'Z',
+}
+
+const majorityElement = (nums) => {
+    const hash = {};
+    let res = 0;
+    let majority = 0;
+
+    for(let n of nums) {
+        hash[n] = 1 + (hash[n] || 0);
+        console.log(hash);
+        if (hash[n] > majority) {
+            res = n;
+            majority = hash[n];
+        }
+    }
+    return res;
+}
