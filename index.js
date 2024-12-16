7bcd7d68c0e47edc7733c542e359b262fe126064
cyrll29 <75314757+cyrll29@users.noreply.github.com>
2024-12-16 14:03:21 +0800

    feat: added ratios of negative, postive, and zeroes of arr function

--- index.js
+++ index.js
@@ -260,5 +260,31 @@ function diagonalDifference(arr) {
     return Math.abs(firstNum - secondNum);
 }
 
+const plusMinus = (arr) => {
+    const res = {
+        positive: 0,
+        negative: 0,
+        zeroes: 0,
+    }
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > 0) {
+            res['positive']++;
+        } else if(arr[i] === 0) {
+            res['zeroes']++;
+        } else {
+            res['negative']++;
+        }
+    }
+    console.log(Object.keys(res).length)
+
+    for(let key in res) {
+        console.log(key + ": " + res[key])
+        console.log(res[key]/arr.length)
+    }
+}
+
+plusMinus([1, 1, 0, -1, -1]);
+
 
 
